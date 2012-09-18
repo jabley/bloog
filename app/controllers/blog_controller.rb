72c5a985ad6fee239837ba77468f95a8167fa049
James Abley <james.abley@gmail.com>
2012-09-18 18:36:16 +0100

    Specs for new_post behaviour

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,12 @@
 class BlogController < ApplicationController
   def index
     @blog = Blog.new
+    post1 = @blog.new_post
+    post1.title = "Paint just applied"
+    post1.body = "Paint just applied. It's a smashing orange!"
+    post1.publish
+    post2 = @blog.new_post(title: "Still wet")
+    post2.body = "Paint is still wet. No bubbles yet though."
+    post2.publish
   end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
@@ -10,5 +11,16 @@ class Blog
   end
   def subtitle
     "The trusted source for drying paint news & opinion"
+  end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+  private
+  def post_source
+    @post_source ||= Post.public_method(:new)
   end
 end

edad8baf6f6b168e2c7556e3c418cb45e51130d9
James Abley <james.abley@gmail.com>
2012-09-21 14:31:48 +0100

    Add new post UI

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,18 @@
 class Post
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
   attr_accessor :title, :body, :blog
+
   def initialize(attrs={})
     attrs.each { |k,v |  send("#{k}=", v)}
   end
+
   def publish
     blog.add_entry(self)
+  end
+
+  def persisted?
+    false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bloog::Application.routes.draw do
   get "blog/index"
 
+  resources :posts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

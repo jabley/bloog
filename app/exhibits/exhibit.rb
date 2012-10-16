f7b2b59373edc8072fc65da2529072aa32a3afa0
James Abley <james.abley@gmail.com>
2012-10-16 09:36:21 +0100

    Refactor exhibits_helper

--- app/exhibits/exhibit.rb
+++ app/exhibits/exhibit.rb
@@ -1,6 +1,13 @@
 require 'delegate'
 
 class Exhibit < SimpleDelegator
+  def self.exhibits
+    [
+    TextPostExhibit,
+    PicturePostExhibit,
+    ]
+  end
+
   def initialize(model, context)
     @context = context
     super(model)
@@ -12,5 +19,23 @@ class Exhibit < SimpleDelegator
 
   def class
     __get_obj__.class
+  end
+
+  def self.applicable_to?(object)
+    false
+  end
+
+  def self.exhibit(object, context)
+    exhibits.inject(object) do |object, exhibit|
+      exhibit.exhibit_if_applicable(object, context)
+    end
+  end
+
+  def self.exhibit_if_applicable(object, context)
+    if applicable_to?(object)
+      new(object, context)
+    else
+      object
+    end
   end
 end
